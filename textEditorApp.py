44f1328b3aeb293d85bbc87a885bfb9ed39b57bf
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-04-28 23:35:40 +0200

    commented previous, started work on welcome screen

--- textEditorApp.py
+++ textEditorApp.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import logging
 import kivy
 
@@ -8,12 +9,22 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.floatlayout import FloatLayout
+
 class base_window(FloatLayout):
     def __init__(self) -> None:
         super().__init__()
-        self.text_input=TextInput(background_color=[0,0,0,1],cursor_color=[0.5,0.5,0.5,1],
-        foreground_color=[1,1,1,1])
-        self.add_widget(self.text_input)
+        self.Welcome_screen()
+        # self.text_input=TextInput(background_color=[0,0,0,1],cursor_color=[0.5,0.5,0.5,1],
+        # foreground_color=[1,1,1,1],font_name="fonts/Helvetica.ttf")
+        # self.add_widget(self.text_input)
+        
+    def Welcome_screen(self):
+        self.name="Welcome screen"
+        self.add_widget(Label(text="Welcome to the Generic Text Editor Project\n\nby LasevIX_",
+        font_name="fonts/Helvetica.ttf",font_size=24,valign="center",halign="center",bold=True,
+        ))
+        start_button=Button(text="Click here to start,\nor press any key")
+        self.add_widget(start_button)
 
 class textEditorApp(App):
     def build(self):
