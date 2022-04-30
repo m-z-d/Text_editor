138247219debeaeeaf81f134ec4ee793f4b1847f
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-04-30 23:57:58 +0200

    figuring out Float layout positioning

--- textEditorApp.py
+++ textEditorApp.py
@@ -27,13 +27,16 @@ class base_window(FloatLayout):
         halign="center",
         bold=True,
         size_hint_y=0.5,
+        size_hint_x=1,
         pos=(0,300),
         )
         self.add_widget(Welcome_label)
         start_button=Button(text="Click here to start,\nor press any key",
-        size_hint_y=0.4,
-        size_hint_x=0.8,
-        pos=(0,0),
+        size_hint_y=0.2,
+        size_hint_x=0.4,
+        pos_hint={"x":0.3,"y":0.2},
+        valign="center",
+        halign="center",
         font_size=24
         )
         self.add_widget(start_button)
