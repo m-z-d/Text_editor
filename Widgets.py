91020c789f9843f493c627751171a046b3d54021
LasevIX_@Laptop-lOhkM <l73yx.off@gmail.com>
2022-05-15 22:54:02 +0200

    added passing of filepath to widget; minor tweaks

--- Widgets.py
+++ Widgets.py
@@ -8,34 +8,61 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.dropdown import DropDown
 from kivy.core.window import Window
 from kivy.properties import *
+from kivy.logger import Logger
 
 class menuBar(GridLayout):
-    def __init__(self, **kwargs):
+    filepath=StringProperty("")
+    filename=StringProperty("filename not defined")
+    def __init__(self, *args, **kwargs):
         super().__init__(**kwargs)
         self.rows=1
         self.size_hint_x=1
-        self.size_hint_y=0.05
+        self.size_hint_y=0.04
 #       ——————————————————————————————
         self.file_menu_button = Button(
             text="File",
             font_size=12
         )
-        self.filemenu=DropDown()
-        self.filemenu_fileselect=Button(
-            text="Open File"
+        self.filemenu=DropDown(
+            auto_width=False,
+            size_hint_x=0.3)
+        self.filemenu_filename=Label(
+            text="Name: "+self.filename,
+            size_hint=(0.9,None),
+            height=25
         )
+        self.filemenu_fileselect=Button(
+            text="Open File",
+            size_hint=(0.9,None),
+            height=25,
+            font_size=12
+        )
+        self.filemenu_properties=Button(
+            text="File Properties",
+            size_hint=(0.9,None),
+            height=25,
+            font_size=12
+        )
+        self.filemenu.add_widget(self.filemenu_filename)
         self.filemenu.add_widget(self.filemenu_fileselect)
-        self.file_menu_button.bind(on_press=self.filemenu.open)
+        self.filemenu.add_widget(self.filemenu_properties)
+        
+        self.file_menu_button.bind(on_release=self.filemenu.open)
         self.insert_menu = Button(
             text="Insert",
             font_size=12
         )
         self.add_widget(self.file_menu_button)
         self.add_widget(self.insert_menu)
+    def on_filepath(self, instance, value):
+        self.filename=value.split("\\")[-1].split("/")[-1]
 
 class view(RelativeLayout):
+    cmd_line_args=ListProperty([])
+    filepath=StringProperty("")
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+        Logger.info(f"Arguments:{self.cmd_line_args}")
         self.Welcome_view()
     def Welcome_view(self):
         username=""
@@ -61,12 +88,19 @@ class view(RelativeLayout):
         )
         start_button.bind(on_press=self.Edit_view)
         self.add_widget(start_button)
-    def Edit_view(self,caller,**kwargs):
+    def Edit_view(self,*args,**kwargs):
+        next_arg_is_filepath=False
         self.clear_widgets()
-        menus=menuBar(pos_hint={"x":0,"y":0.95})
+        for i in self.cmd_line_args:
+            if next_arg_is_filepath:
+                self.filepath=i
+                Logger.info(f"filepath received: {self.filepath}")
+            if i =="-f":
+                next_arg_is_filepath=True
+        menus=menuBar(pos_hint={"x":0,"y":0.96},filepath=self.filepath)
         self.add_widget(menus)
         textinput=TextInput(
-            size_hint=(0.85,0.9),
+            size_hint=(0.85,0.96),
             pos_hint={"x":0.25,"y":0},
             background_color= [0.3,0.3,0.3,1]
 
--- textEditorApp.py
+++ textEditorApp.py
@@ -1,13 +1,15 @@
 import logging
+import sys
 import kivy
 import Widgets
 from kivy.app import App
 from kivy.properties import *
 
+
 class textEditorApp(App):
     def build(self):
-        return Widgets.view()
+        return Widgets.view(cmd_line_args=sys.argv[1:])
 
 if __name__=="__main__":
     textEditorApp().run()
-
+    
